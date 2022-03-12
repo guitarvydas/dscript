731690bf2ef5514427efa07c15ff5fa401381abe
Paul Tarvydas <guitarvydas@gmail.com>
2022-03-12 13:31:51 -0500

    mkfifo

--- support.js
+++ support.js
@@ -1,8 +1,18 @@
 var componentPath = [];
-function cpath () { return componentPath.join ('_'); };
+
+exports.resetNames = function () { componentPath = []; }
+
+function cpath () { 
+    let path = componentPath.join ('_');
+    if (path) {
+	return path + "_";
+    } else {
+	return "";
+    }
+}	
 
 exports.mangleChildName = function (s) {
-    return cpath () + "_" + s.replace (/ /g, "_").replace (/\//g, "__");
+    return cpath () + s.replace (/ /g, "_").replace (/\//g, "__").replace (/\$/g,"___");
 }
 
 exports.pushComponentName = function (s) {
@@ -18,4 +28,8 @@ exports.popComponentName = function () {
 exports.panic_not_allowed_in_bash = function () {
     console.error ("*** ERROR: copying to multiple receivers not available in BASH");
     throw "ERROR";
+}
+
+exports.manglePortName = function (s) {
+    return s.replace (/\$/g, "___");
 }
