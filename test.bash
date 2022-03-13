ff1ea9dd316b2f5961bc5baa95a4f6255f6f8c18
Paul Tarvydas <guitarvydas@gmail.com>
2022-03-13 07:11:45 -0400

    cp before removing mangleChildName

--- test.bash
+++ test.bash
@@ -1,7 +1,6 @@
 cat >clone__clean.bash <<'~~~'
 
 # child clone__clean_clean_1
-
 duct_clone__clean_clean_1___in=/tmp/duct_clone__clean_clean_1___in
 duct_clone__clean_clean_1___out=/tmp/duct_clone__clean_clean_1___out
 clone__clean_clean_1 <${duct_clone__clean_clean_1___in} >${duct_clone__clean_clean_1___out} &
@@ -9,7 +8,6 @@ pid_clone__clean_clean_1=$!
 
 
 # child clone__clean_clean_2
-
 duct_clone__clean_clean_2___in=/tmp/duct_clone__clean_clean_2___in
 duct_clone__clean_clean_2___out=/tmp/duct_clone__clean_clean_2___out
 clone__clean_clean_2 <${duct_clone__clean_clean_2___in} >${duct_clone__clean_clean_2___out} &
@@ -17,9 +15,9 @@ pid_clone__clean_clean_2=$!
 
 
 # connections
-cat $1 >clone__clean_clean_1____in &
-cat <clean 1___out >clone__clean_clean_2____in &
-cat <clean 2___out
+cat $1 >${duct_clone__clean_clean_1____in} &
+cat <${duct_clean 1___out} >${duct_clone__clean_clean_2____in} &
+cat <${duct_clean 2___out}
 
 
 ~~~
